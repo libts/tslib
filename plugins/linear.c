2a25f44950643417bd36e33e5038fefb927afa02
Douglas Lowder <douglowder@mac.com>
2003-03-05 23:03:04 -0700

    Fix to swap_xy initialization in plugins/linear.c (contributed by Eric Nelson)

--- plugins/linear.c
+++ plugins/linear.c
@@ -6,7 +6,7 @@
  * This file is placed under the LGPL.  Please see the file
  * COPYING for more details.
  *
- * $Id: linear.c,v 1.7 2003/03/04 17:09:47 dlowder Exp $
+ * $Id: linear.c,v 1.8 2003/03/05 23:03:04 dlowder Exp $
  *
  * Linearly scale touchscreen values
  */
@@ -60,11 +60,11 @@ linear_read(struct tslib_module_info *info, struct ts_sample *samp, int nr)
 
 			samp->pressure = ((samp->pressure + lin->p_offset)
 						 * lin->p_mult) / lin->p_div;
-	//		if (lin->swap_xy) {
-	//			int tmp = samp->x;
-	//			samp->x = samp->y;
-	//			samp->y = tmp;
-	//		}
+			if (lin->swap_xy) {
+				int tmp = samp->x;
+				samp->x = samp->y;
+				samp->y = tmp;
+			}
 		}
 	}
 
@@ -128,6 +128,7 @@ struct tslib_module_info *mod_init(struct tsdev *dev, const char *params)
 	lin->p_offset = 0;
 	lin->p_mult   = 1;
 	lin->p_div    = 1;
+	lin->swap_xy  = 0;
 
 	/*
 	 * Check calibration file
