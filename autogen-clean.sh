550322641e901a5adc10981e6d545352c7e35981
Chris Larson <clarson@kergoth.com>
2005-02-26 02:06:20 -0700

    Update autogen-clean.sh to add a few more files to remove.

--- autogen-clean.sh
+++ autogen-clean.sh
@@ -4,6 +4,5 @@ if [ -f Makefile ]; then
 	make distclean
 fi
 echo "Removing autogenned files..."
-rm -f config.guess config.sub configure install-sh missing mkinstalldirs Makefile.in ltmain.sh stamp-h.in */Makefile.in ltconfig
+rm -f config.guess config.sub configure install-sh missing mkinstalldirs Makefile.in ltmain.sh stamp-h.in */Makefile.in ltconfig stamp-h config.h.in
 echo "Done."
-#./configure $*
