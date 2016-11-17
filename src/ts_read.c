c098392da2f53df143d365997dc8d9e99680b359
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2016-11-17 09:43:51 +0100

    ts_read: remove deprecated, old comments
    We use user-buffers. No need to confuse anyone by having these 14 years
    old comments here.

--- src/ts_read.c
+++ src/ts_read.c
@@ -19,25 +19,11 @@
 #include <string.h>
 #endif
 
-/* This array is used to prevent segfaults and memory overwrites
- * that can occur if multiple events are returned from ts_read_raw
- * for each event returned by ts_read
- */
-/* We found this was not needed, and have gone back to the
- * original implementation
- */
-
-// static struct ts_sample ts_read_private_samples[1024];
-
 int ts_read(struct tsdev *ts, struct ts_sample *samp, int nr)
 {
 	int result;
-//	int i;
-//	result = ts->list->ops->read(ts->list, ts_read_private_samples, nr);
+
 	result = ts->list->ops->read(ts->list, samp, nr);
-//	for(i=0;i<nr;i++) {
-//		samp[i] = ts_read_private_samples[i];
-//	}
 #ifdef DEBUG
 	if (result)
 		fprintf(stderr,"TS_READ----> x = %d, y = %d, pressure = %d\n", samp->x, samp->y, samp->pressure);
