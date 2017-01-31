4bd92373d6ba7394b6ec997ae1f965b0d3a3fa51
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-01-31 16:42:02 +0100

    input-raw: define the ABS_MT_ set of values if ABS_MT_POSITION_X is missing
    We don't require multitouch to be there, so at this point we have to add the
    complete set of definitions from input.h.

--- plugins/input-raw.c
+++ plugins/input-raw.c
@@ -58,8 +58,21 @@
 #endif
 
 #ifndef ABS_MT_POSITION_X
-# define ABS_MT_POSITION_X       0x35    /* Center X ellipse position */
-# define ABS_MT_POSITION_Y       0x36    /* Center Y ellipse position */
+# define ABS_MT_SLOT             0x2f    /* MT slot being modified */
+# define ABS_MT_TOUCH_MAJOR      0x30    /* Major axis of touching ellipse */
+# define ABS_MT_TOUCH_MINOR      0x31    /* Minor axis (omit if circular) */
+# define ABS_MT_WIDTH_MAJOR      0x32    /* Major axis of approaching ellipse */
+# define ABS_MT_WIDTH_MINOR      0x33    /* Minor axis (omit if circular) */
+# define ABS_MT_ORIENTATION      0x34    /* Ellipse orientation */
+# define ABS_MT_POSITION_X       0x35    /* Center X touch position */
+# define ABS_MT_POSITION_Y       0x36    /* Center Y touch position */
+# define ABS_MT_TOOL_TYPE        0x37    /* Type of touching device */
+# define ABS_MT_BLOB_ID          0x38    /* Group a set of packets as a blob */
+# define ABS_MT_TRACKING_ID      0x39    /* Unique ID of initiated contact */
+# define ABS_MT_PRESSURE         0x3a    /* Pressure on contact area */
+# define ABS_MT_DISTANCE         0x3b    /* Contact hover distance */
+# define ABS_MT_TOOL_X           0x3c    /* Center X tool position */
+# define ABS_MT_TOOL_Y           0x3d    /* Center Y tool position */
 #endif
 
 #include "tslib-private.h"
