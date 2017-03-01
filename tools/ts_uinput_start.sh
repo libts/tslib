a5e60331a89152a9e42a758b3804605c5486def4
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-03-01 16:31:45 +0100

    tools: improve ts_uinput_start.sh
    Don't print anything while succeeding and catch all ts_uinput errors.

--- tools/ts_uinput_start.sh
+++ tools/ts_uinput_start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 TS_UINPUT_SYSFS_NAME=$(ts_uinput -d)
-if [ "$TS_UINPUT_SYSFS_NAME" == "" ] ; then
+if [[ "${TS_UINPUT_SYSFS_NAME}" != *"input"* ]] ; then
+	echo "ts_uinput: failed to create uinput device"
 	exit 1
 fi
 
-if [ -d "/sys/devices/virtual/input/$TS_UINPUT_SYSFS_NAME" ] ; then
-	TS_UINPUT_DEV_FILE=$(ls /sys/devices/virtual/input/$TS_UINPUT_SYSFS_NAME | grep '^event')
-	ln -s /dev/input/$TS_UINPUT_DEV_FILE /dev/input/ts_uinput
-	exit 0
-else
-	echo "ts_uinput: Error creating /dev/input/ts_uinput" >$2
+TS_UINPUT_SYSFS_PATH="/sys/devices/virtual/input/${TS_UINPUT_SYSFS_NAME}"
+if [ ! -d "${TS_UINPUT_SYSFS_PATH}" ] ; then
+	echo "ts_uinput: Error creating event device"
 	exit 1
 fi
+
+TS_UINPUT_DEV_FILE=$(ls /sys/devices/virtual/input/$TS_UINPUT_SYSFS_NAME | grep '^event')
+ln -s /dev/input/$TS_UINPUT_DEV_FILE /dev/input/ts_uinput
