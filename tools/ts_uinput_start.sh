00748e159513bf2913d62c0720cf81633237311f
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-03-02 10:51:02 +0100

    ts_uinput_start.sh: fix error checking

--- tools/ts_uinput_start.sh
+++ tools/ts_uinput_start.sh
@@ -4,16 +4,19 @@
 # as an evdev input device
 
 TS_UINPUT_SYSFS_NAME=$(ts_uinput -d)
-if [[ "${TS_UINPUT_SYSFS_NAME}" != *"input"* ]] ; then
-	echo "ts_uinput: failed to create uinput device"
-	exit 1
+TS_UINPUT_CHECK="input"
+if [ "${TS_UINPUT_SYSFS_NAME}/$TS_UINPUT_CHECK" = "${TS_UINPUT_SYSFS_NAME}" ]
+then
+        echo "ts_uinput: failed to create uinput device ${TS_UINPUT_SYSFS_NAME}"
+        exit 1
 fi
-
 TS_UINPUT_SYSFS_PATH="/sys/devices/virtual/input/${TS_UINPUT_SYSFS_NAME}"
 if [ ! -d "${TS_UINPUT_SYSFS_PATH}" ] ; then
-	echo "ts_uinput: Error creating event device"
-	exit 1
+        echo "ts_uinput: Error creating event device"
+        exit 1
 fi
 
+rm -f /dev/input/ts_uinput
 TS_UINPUT_DEV_FILE=$(ls /sys/devices/virtual/input/$TS_UINPUT_SYSFS_NAME | grep '^event')
 ln -s /dev/input/$TS_UINPUT_DEV_FILE /dev/input/ts_uinput
+exit 0
