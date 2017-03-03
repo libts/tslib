1b52c3acd128f1c2e692ff5a83ab56fda1570328
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-03-03 08:20:52 +0100

    autogen-clean.sh: add missing generated files

--- autogen-clean.sh
+++ autogen-clean.sh
@@ -4,5 +4,5 @@ if [ -f Makefile ]; then
 	make distclean
 fi
 echo "Removing autogenned files..."
-rm -f config.guess config.sub configure install-sh missing mkinstalldirs Makefile.in ltmain.sh stamp-h.in */Makefile.in ltconfig stamp-h config.h.in
+rm -f config.guess config.sub configure install-sh missing mkinstalldirs Makefile.in ltmain.sh stamp-h.in */Makefile.in ltconfig stamp-h config.h.in* aclocal.m4 compile depcomp
 echo "Done."
