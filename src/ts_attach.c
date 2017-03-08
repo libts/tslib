26e721ec704c1cc53e9f9996c5b8782e8db8d890
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-03-08 12:57:44 +0100

    ts_attach: coding style fixes

--- src/ts_attach.c
+++ src/ts_attach.c
@@ -27,23 +27,24 @@ int __ts_attach(struct tsdev *ts, struct tslib_module_info *info)
 int __ts_attach_raw(struct tsdev *ts, struct tslib_module_info *info)
 {
 	struct tslib_module_info *next, *prev, *prev_list = ts->list_raw;
+
 	info->dev = ts;
 	info->next = prev_list;
 	ts->list_raw = info;
 
-	/* 
+	/*
 	 * ensure the last item in the normal list now points to the
 	 * top of the raw list.
 	 */
-
-	if (ts->list == NULL || ts->list == prev_list) { /* main list is empty, ensure it points here */
+	if (ts->list == NULL || ts->list == prev_list) {
+		/* main list is empty, ensure it points here */
 		ts->list = info;
 		return 0;
 	}
 
-	for(next = ts->list, prev=next;
-	    next != NULL && next != prev_list;
-	    next = prev->next, prev = next);
+	for (next = ts->list, prev = next;
+	     next != NULL && next != prev_list;
+	     next = prev->next, prev = next);
 
 	prev->next = info;
 	return 0;
