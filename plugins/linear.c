e141bd2e662995b918c2d1a75015eb452c4cc486
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-03-09 10:07:41 +0100

    linear: coding style fixes

--- plugins/linear.c
+++ plugins/linear.c
@@ -40,12 +40,12 @@ struct tslib_linear {
 	unsigned int cal_res_y;
 };
 
-static int
-linear_read(struct tslib_module_info *info, struct ts_sample *samp, int nr)
+static int linear_read(struct tslib_module_info *info, struct ts_sample *samp,
+		       int nr)
 {
 	struct tslib_linear *lin = (struct tslib_linear *)info;
 	int ret;
-	int xtemp,ytemp;
+	int xtemp, ytemp;
 
 	ret = info->next->ops->read(info->next, samp, nr);
 	if (ret >= 0) {
@@ -53,22 +53,26 @@ linear_read(struct tslib_module_info *info, struct ts_sample *samp, int nr)
 
 		for (nr = 0; nr < ret; nr++, samp++) {
 		#ifdef DEBUG
-			fprintf(stderr,"BEFORE CALIB--------------------> %d %d %d\n",samp->x, samp->y, samp->pressure);
+			fprintf(stderr,
+				"BEFORE CALIB--------------------> %d %d %d\n",
+				samp->x, samp->y, samp->pressure);
 		#endif /* DEBUG */
 			xtemp = samp->x; ytemp = samp->y;
-			samp->x = 	( lin->a[2] +
-					lin->a[0]*xtemp + 
-					lin->a[1]*ytemp ) / lin->a[6];
-			samp->y =	( lin->a[5] +
+			samp->x = 	(lin->a[2] +
+					lin->a[0]*xtemp +
+					lin->a[1]*ytemp) / lin->a[6];
+			samp->y =	(lin->a[5] +
 					lin->a[3]*xtemp +
-					lin->a[4]*ytemp ) / lin->a[6];
+					lin->a[4]*ytemp) / lin->a[6];
 			if (info->dev->res_x && lin->cal_res_x)
-				samp->x = samp->x * info->dev->res_x / lin->cal_res_x;
+				samp->x = samp->x * info->dev->res_x
+					  / lin->cal_res_x;
 			if (info->dev->res_y && lin->cal_res_y)
-				samp->y = samp->y * info->dev->res_y / lin->cal_res_y;
+				samp->y = samp->y * info->dev->res_y
+					  / lin->cal_res_y;
 
 			samp->pressure = ((samp->pressure + lin->p_offset)
-						 * lin->p_mult) / lin->p_div;
+					  * lin->p_mult) / lin->p_div;
 			if (lin->swap_xy) {
 				int tmp = samp->x;
 				samp->x = samp->y;
@@ -80,51 +84,60 @@ linear_read(struct tslib_module_info *info, struct ts_sample *samp, int nr)
 	return ret;
 }
 
-static int
-linear_read_mt(struct tslib_module_info *info, struct ts_sample_mt **samp, int max_slots, int nr_samples)
+static int linear_read_mt(struct tslib_module_info *info,
+			  struct ts_sample_mt **samp,
+			  int max_slots, int nr_samples)
 {
 	struct tslib_linear *lin = (struct tslib_linear *)info;
 	int ret;
-	int xtemp,ytemp;
+	int xtemp, ytemp;
 	int i;
 	int nr;
 
 	ret = info->next->ops->read_mt(info->next, samp, max_slots, nr_samples);
 	if (ret < 0) {
 	#ifdef DEBUG
-		fprintf(stderr,"linear: couldn't read a sample\n");
+		fprintf(stderr, "linear: couldn't read a sample\n");
 	#endif /*DEBUG*/
 		return ret;
 	}
 
 	for (nr = 0; nr < ret; nr++) {
 	#ifdef DEBUG
-		printf("LINEAR: read %d samples (mem: %d nr x %d slots)\n", ret, nr_samples, max_slots);
-		fprintf(stderr,"BEFORE CALIB-----------------------v\n");
+		printf("LINEAR: read %d samples (mem: %d nr x %d slots)\n",
+		       ret, nr_samples, max_slots);
+		fprintf(stderr, "BEFORE CALIB-----------------------v\n");
 	#endif /*DEBUG*/
 		for (i = 0; i < max_slots; i++) {
 			if (samp[nr][i].valid != 1)
 				continue;
 
 		#ifdef DEBUG
-			fprintf(stderr,"          slot %d            %d %d %d\n",
-				i, samp[nr][i].x, samp[nr][i].y, samp[nr][i].pressure);
+			fprintf(stderr, "          slot %d            %d %d %d\n",
+				i, samp[nr][i].x, samp[nr][i].y,
+				samp[nr][i].pressure);
 		#endif /*DEBUG*/
 			xtemp = samp[nr][i].x;
 			ytemp = samp[nr][i].y;
-			samp[nr][i].x =	( lin->a[2] +
-					lin->a[0]*xtemp + 
-					lin->a[1]*ytemp ) / lin->a[6];
-			samp[nr][i].y =	( lin->a[5] +
+			samp[nr][i].x =	(lin->a[2] +
+					lin->a[0]*xtemp +
+					lin->a[1]*ytemp) / lin->a[6];
+			samp[nr][i].y =	(lin->a[5] +
 					lin->a[3]*xtemp +
-					lin->a[4]*ytemp ) / lin->a[6];
+					lin->a[4]*ytemp) / lin->a[6];
 			if (info->dev->res_x && lin->cal_res_x)
-				samp[nr][i].x = samp[nr][i].x * info->dev->res_x / lin->cal_res_x;
+				samp[nr][i].x = samp[nr][i].x *
+						info->dev->res_x /
+						lin->cal_res_x;
 			if (info->dev->res_y && lin->cal_res_y)
-				samp[nr][i].y = samp[nr][i].y * info->dev->res_y / lin->cal_res_y;
+				samp[nr][i].y = samp[nr][i].y *
+						info->dev->res_y /
+						lin->cal_res_y;
 
-			samp[nr][i].pressure = ((samp[nr][i].pressure + lin->p_offset)
-						 * lin->p_mult) / lin->p_div;
+			samp[nr][i].pressure = ((samp[nr][i].pressure +
+						 lin->p_offset) *
+						 lin->p_mult) /
+						 lin->p_div;
 			if (lin->swap_xy) {
 				int tmp = samp[nr][i].x;
 				samp[nr][i].x = samp[nr][i].y;
@@ -142,8 +155,7 @@ static int linear_fini(struct tslib_module_info *info)
 	return 0;
 }
 
-static const struct tslib_ops linear_ops =
-{
+static const struct tslib_ops linear_ops = {
 	.read		= linear_read,
 	.read_mt	= linear_read_mt,
 	.fini		= linear_fini,
@@ -166,7 +178,7 @@ static int linear_p_offset(struct tslib_module_info *inf, char *str,
 
 	unsigned long offset = strtoul(str, NULL, 0);
 
-	if(offset == ULONG_MAX && errno == ERANGE)
+	if (offset == ULONG_MAX && errno == ERANGE)
 		return -1;
 
 	lin->p_offset = offset;
@@ -179,7 +191,7 @@ static int linear_p_mult(struct tslib_module_info *inf, char *str,
 	struct tslib_linear *lin = (struct tslib_linear *)inf;
 	unsigned long mult = strtoul(str, NULL, 0);
 
-	if(mult == ULONG_MAX && errno == ERANGE)
+	if (mult == ULONG_MAX && errno == ERANGE)
 		return -1;
 
 	lin->p_mult = mult;
@@ -192,19 +204,18 @@ static int linear_p_div(struct tslib_module_info *inf, char *str,
 	struct tslib_linear *lin = (struct tslib_linear *)inf;
 	unsigned long div = strtoul(str, NULL, 0);
 
-	if(div == ULONG_MAX && errno == ERANGE)
+	if (div == ULONG_MAX && errno == ERANGE)
 		return -1;
 
 	lin->p_div = div;
 	return 0;
 }
 
-static const struct tslib_vars linear_vars[] =
-{
+static const struct tslib_vars linear_vars[] = {
 	{ "xyswap",	(void *)1, linear_xyswap },
-        { "pressure_offset", NULL , linear_p_offset},
-        { "pressure_mul", NULL, linear_p_mult},
-        { "pressure_div", NULL, linear_p_div},
+	{ "pressure_offset", NULL, linear_p_offset},
+	{ "pressure_mul", NULL, linear_p_mult},
+	{ "pressure_div", NULL, linear_p_div},
 };
 
 #define NR_VARS (sizeof(linear_vars) / sizeof(linear_vars[0]))
@@ -241,8 +252,10 @@ TSAPI struct tslib_module_info *linear_mod_init(__attribute__ ((unused)) struct 
 	/*
 	 * Check calibration file
 	 */
-	if( (calfile = getenv("TSLIB_CALIBFILE")) == NULL) calfile = TS_POINTERCAL;
-	if (stat(calfile, &sbuf)==0) {
+	if ((calfile = getenv("TSLIB_CALIBFILE")) == NULL)
+		calfile = TS_POINTERCAL;
+
+	if (stat(calfile, &sbuf) == 0) {
 		pcal_fd = fopen(calfile, "r");
 		if (!pcal_fd) {
 			free(lin);
@@ -251,20 +264,22 @@ TSAPI struct tslib_module_info *linear_mod_init(__attribute__ ((unused)) struct 
 		}
 
 		for (index = 0; index < 7; index++)
-			if (fscanf(pcal_fd, "%d", &lin->a[index]) != 1) break;
+			if (fscanf(pcal_fd, "%d", &lin->a[index]) != 1)
+				break;
 
 		if (!fscanf(pcal_fd, "%d %d", &lin->cal_res_x, &lin->cal_res_y))
-			fprintf(stderr, "LINEAR: Couldn't read resolution values\n");
+			fprintf(stderr,
+				"LINEAR: Couldn't read resolution values\n");
 
 #ifdef DEBUG
 		printf("Linear calibration constants: ");
-		for(index=0;index<7;index++) printf("%d ",lin->a[index]);
+		for (index = 0; index < 7; index++)
+			printf("%d ", lin->a[index]);
 		printf("\n");
 #endif /*DEBUG*/
 		fclose(pcal_fd);
 	}
-		
-		
+
 	/*
 	 * Parse the parameters.
 	 */
