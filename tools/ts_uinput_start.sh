b776f00068975f2fdfb2394dc33bac308dd8017d
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-04-04 11:08:20 +0200

    ts_uinput_start.sh: use ts_uinput -d -v

--- tools/ts_uinput_start.sh
+++ tools/ts_uinput_start.sh
@@ -3,20 +3,15 @@
 # This starts ts_uinput as a daemon and creates /dev/input/ts_uinput to use
 # as an evdev input device
 
-TS_UINPUT_SYSFS_NAME=$(ts_uinput -d)
-TS_UINPUT_CHECK="input"
-if [ "${TS_UINPUT_SYSFS_NAME}/$TS_UINPUT_CHECK" = "${TS_UINPUT_SYSFS_NAME}" ]
+rm -f /dev/input/ts_uinput
+TS_UINPUT_DEV_FILE=$(ts_uinput -d -v)
+
+if [ -z "$TS_UINPUT_DEV_FILE" ]
 then
-        echo "ts_uinput: failed to create uinput device ${TS_UINPUT_SYSFS_NAME}"
-        exit 1
-fi
-TS_UINPUT_SYSFS_PATH="/sys/devices/virtual/input/${TS_UINPUT_SYSFS_NAME}"
-if [ ! -d "${TS_UINPUT_SYSFS_PATH}" ] ; then
-        echo "ts_uinput: Error creating event device"
-        exit 1
+	echo "ts_uinput: Error creating event device"
+	exit 1
+else
+	ln -s /dev/input/$TS_UINPUT_DEV_FILE /dev/input/ts_uinput
 fi
 
-rm -f /dev/input/ts_uinput
-TS_UINPUT_DEV_FILE=$(ls /sys/devices/virtual/input/$TS_UINPUT_SYSFS_NAME | grep '^event')
-ln -s /dev/input/$TS_UINPUT_DEV_FILE /dev/input/ts_uinput
 exit 0
