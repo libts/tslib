c03f7c9ea3b91cfff85a697ac0aac68647ba13ee
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-04-21 12:11:07 +0200

    tools: ts_uinput_start.sh: fix usage of ts_uinput -d -v

--- tools/ts_uinput_start.sh
+++ tools/ts_uinput_start.sh
@@ -3,15 +3,16 @@
 # This starts ts_uinput as a daemon and creates /dev/input/ts_uinput to use
 # as an evdev input device
 
-rm -f /dev/input/ts_uinput
 TS_UINPUT_DEV_FILE=$(ts_uinput -d -v)
+TS_UINPUT_DEV_FILE_CHECKED=$(ls "$TS_UINPUT_DEV_FILE" | grep 'event')
 
-if [ -z "$TS_UINPUT_DEV_FILE" ]
+if [ ! -z "$TS_UINPUT_DEV_FILE_CHECKED" ]
 then
+	rm -f /dev/input/ts_uinput
+	ln -s $TS_UINPUT_DEV_FILE_CHECKED /dev/input/ts_uinput
+else
 	echo "ts_uinput: Error creating event device"
 	exit 1
-else
-	ln -s /dev/input/$TS_UINPUT_DEV_FILE /dev/input/ts_uinput
 fi
 
 exit 0
