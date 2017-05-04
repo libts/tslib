6a4b8059e6190fb2d1eb0714cbde2b7f841e12b7
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-05-04 11:47:43 +0200

    ts_option: avoid returning an uninitialized value

--- src/ts_option.c
+++ src/ts_option.c
@@ -22,16 +22,15 @@ int ts_option(struct tsdev *ts, enum ts_param param, ...)
 	va_list ap;
 
 	va_start(ap, param);
+	ret = 0;
 
 	switch (param) {
 	case TS_SCREEN_RES:
 		ts->res_x = va_arg(ap, unsigned int);
 		ts->res_y = va_arg(ap, unsigned int);
-		ret = 0;
 		break;
 	case TS_SCREEN_ROT:
 		ts->rotation = va_arg(ap, int);
-		ret = 0;
 		break;
 	}
 	va_end(ap);
