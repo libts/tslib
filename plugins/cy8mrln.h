580601e895bd475da202c7d90400c80d70531118
Martin Kepplinger <martink@posteo.de>
2017-05-08 21:44:04 +0200

    cy8mrln.h: remove wrong FSF address and update to what fsf.org suggests

--- plugins/cy8mrln.h
+++ plugins/cy8mrln.h
@@ -5,20 +5,13 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2 of the License.
  *
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __CY8MRLN_H
