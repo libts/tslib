e982e6c67db5a416ff5643cb8fd87c0c51ff507d
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-10-20 09:07:05 +0200

    autogen.sh: remove dead code
    Signed-off-by: Martin Kepplinger <martin.kepplinger@ginzinger.com>

--- autogen.sh
+++ autogen.sh
@@ -2,21 +2,3 @@
 
 autoreconf -f -i -I $(pwd)/m4
 exit $?
-
-echo -n "Libtoolize..."
-libtoolize --force --copy
-echo "Done."
-echo -n "Aclocal..."
-aclocal
-echo "Done."
-echo -n "Autoheader..."
-autoheader
-echo "Done."
-echo -n "Automake..."
-automake --add-missing --copy
-echo "Done."
-echo -n "Autoconf..."
-autoconf
-echo "Done."
-#./configure $*
-echo "Now you can do ./configure, make, make install."
