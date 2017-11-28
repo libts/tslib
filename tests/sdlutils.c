2e970ee62e9081d1b82a07e9eb687dd24ca34077
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-11-28 14:22:46 +0100

    sdlutils: coding style fixes
    Signed-off-by: Martin Kepplinger <martin.kepplinger@ginzinger.com>

--- tests/sdlutils.c
+++ tests/sdlutils.c
@@ -97,12 +97,12 @@ void draw_crosshair(SDL_Renderer *r, int32_t x, int32_t y)
 	draw_line(r, x + 8, y - 9, x + 6, y - 9);
 }
 
-static int sort_by_x(const void* a, const void *b)
+static int sort_by_x(const void *a, const void *b)
 {
 	return (((struct ts_sample *)a)->x - ((struct ts_sample *)b)->x);
 }
 
-static int sort_by_y(const void* a, const void *b)
+static int sort_by_y(const void *a, const void *b)
 {
 	return (((struct ts_sample *)a)->y - ((struct ts_sample *)b)->y);
 }
@@ -118,7 +118,6 @@ void getxy(struct tsdev *ts, int *x, int *y)
 			perror("ts_read");
 			SDL_Quit();
 		}
-		
 	} while (samp[0].pressure == 0);
 
 	/* Now collect up to MAX_SAMPLES touches into the samp array. */
@@ -131,7 +130,7 @@ void getxy(struct tsdev *ts, int *x, int *y)
 			SDL_Quit();
 		}
 	} while (samp[index].pressure > 0);
-	printf("Took %d samples...\n",index);
+	printf("Took %d samples...\n", index);
 
 	/*
 	 * At this point, we have samples in indices zero to (index-1)
