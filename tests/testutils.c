f20fe3db7714f3869e211cca1d222b062395246e
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-11-28 14:27:23 +0100

    testutils: coding style fixes
    Signed-off-by: Martin Kepplinger <martin.kepplinger@ginzinger.com>

--- tests/testutils.c
+++ tests/testutils.c
@@ -22,27 +22,26 @@
 #include "testutils.h"
 
 /* [inactive] border fill text [active] border fill text */
-static int button_palette [6] =
-{
-        1, 4, 2,
-        1, 5, 0
+static int button_palette[6] = {
+	1, 4, 2,
+	1, 5, 0
 };
 
-void button_draw (struct ts_button *button)
+void button_draw(struct ts_button *button)
 {
 	int s = (button->flags & BUTTON_ACTIVE) ? 3 : 0;
 
 	rect(button->x, button->y, button->x + button->w,
-	     button->y + button->h, button_palette [s]);
+	     button->y + button->h, button_palette[s]);
 	fillrect(button->x + 1, button->y + 1,
 		 button->x + button->w - 2,
-		 button->y + button->h - 2, button_palette [s + 1]);
+		 button->y + button->h - 2, button_palette[s + 1]);
 	put_string_center(button->x + button->w / 2,
 			  button->y + button->h / 2,
-			  button->text, button_palette [s + 2]);
+			  button->text, button_palette[s + 2]);
 }
 
-int button_handle (struct ts_button *button, int x, int y, unsigned int p)
+int button_handle(struct ts_button *button, int x, int y, unsigned int p)
 {
 	int inside = (x >= button->x) && (y >= button->y) &&
 		     (x < button->x + button->w) &&
@@ -52,33 +51,34 @@ int button_handle (struct ts_button *button, int x, int y, unsigned int p)
 		if (inside) {
 			if (!(button->flags & BUTTON_ACTIVE)) {
 				button->flags |= BUTTON_ACTIVE;
-				button_draw (button);
+				button_draw(button);
 			}
 		} else if (button->flags & BUTTON_ACTIVE) {
 			button->flags &= ~BUTTON_ACTIVE;
-			button_draw (button);
+			button_draw(button);
 		}
 	} else if (button->flags & BUTTON_ACTIVE) {
 		button->flags &= ~BUTTON_ACTIVE;
-		button_draw (button);
+		button_draw(button);
 		return 1;
 	}
 
 	return 0;
 }
 
-static int sort_by_x(const void* a, const void *b)
+static int sort_by_x(const void *a, const void *b)
 {
 	return (((struct ts_sample *)a)->x - ((struct ts_sample *)b)->x);
 }
 
-static int sort_by_y(const void* a, const void *b)
+static int sort_by_y(const void *a, const void *b)
 {
 	return (((struct ts_sample *)a)->y - ((struct ts_sample *)b)->y);
 }
 
 /* Waits for the screen to be touched, averages x and y sample
- * coordinates until the end of contact */
+ * coordinates until the end of contact
+ */
 void getxy(struct tsdev *ts, int *x, int *y)
 {
 #define MAX_SAMPLES 128
@@ -88,10 +88,9 @@ void getxy(struct tsdev *ts, int *x, int *y)
 	do {
 		if (ts_read_raw(ts, &samp[0], 1) < 0) {
 			perror("ts_read");
-			close_framebuffer ();
+			close_framebuffer();
 			exit(1);
 		}
-		
 	} while (samp[0].pressure == 0);
 
 	/* Now collect up to MAX_SAMPLES touches into the samp array. */
@@ -101,11 +100,11 @@ void getxy(struct tsdev *ts, int *x, int *y)
 			index++;
 		if (ts_read_raw(ts, &samp[index], 1) < 0) {
 			perror("ts_read");
-			close_framebuffer ();
+			close_framebuffer();
 			exit(1);
 		}
 	} while (samp[index].pressure > 0);
-	printf("Took %d samples...\n",index);
+	printf("Took %d samples...\n", index);
 
 	/*
 	 * At this point, we have samples in indices zero to (index-1)
@@ -142,7 +141,7 @@ void getxy(struct tsdev *ts, int *x, int *y)
 	}
 }
 
-void ts_flush (struct tsdev *ts)
+void ts_flush(struct tsdev *ts)
 {
 	/* Read all unread touchscreen data,
 	 * so that we are sure that the next data that we read
@@ -151,7 +150,8 @@ void ts_flush (struct tsdev *ts)
 
 #define TS_BUFFER_MAX 32768
 	static char buffer[TS_BUFFER_MAX];
-	if (read (ts_fd (ts), buffer, TS_BUFFER_MAX) == -1)
+
+	if (read(ts_fd(ts), buffer, TS_BUFFER_MAX) == -1)
 		fprintf(stderr, "ts_flush read error\n");
 }
 
--- tests/testutils.h
+++ tests/testutils.h
@@ -29,7 +29,7 @@ struct ts_button {
 void button_draw(struct ts_button *button);
 int button_handle(struct ts_button *button, int x, int y, unsigned int pressure);
 void getxy(struct tsdev *ts, int *x, int *y);
-void ts_flush (struct tsdev *ts);
+void ts_flush(struct tsdev *ts);
 void print_ascii_logo(void);
 
 #endif /* _TESTUTILS_H */
