d8f513474feef4f6e71aaa74330873bccd03f087
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2018-04-17 10:39:33 +0200

    add TSLIB_VERSION_VERSION feature definition for the version functions

--- src/tslib.h
+++ src/tslib.h
@@ -108,6 +108,7 @@ struct ts_lib_version_data {
 #define TSLIB_VERSION_MT		(1 << 0)	/* multitouch support */
 #define TSLIB_VERSION_OPEN_RESTRICTED	(1 << 1)	/* ts_open_restricted() */
 #define TSLIB_VERSION_EVENTPATH		(1 << 2)	/* ts_get_eventpath() */
+#define TSLIB_VERSION_VERSION		(1 << 3)	/* tslib_version() */
 
 enum ts_param {
 	TS_SCREEN_RES = 0,		/* 2 integer args, x and y */
