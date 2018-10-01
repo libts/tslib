b09a4fa4a6d5a499afbef25a6d1f5cfa87ffc8f9
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2018-10-01 07:00:05 +0200

    tslib 1.18-rc1+

--- src/ts_version.c
+++ src/ts_version.c
@@ -17,7 +17,7 @@
 #include "config.h"
 #include "tslib.h"
 
-#define LIBTS_DATESTAMP "2018-10-01"
+#define LIBTS_DATESTAMP "[unreleased]"
 
 static struct ts_lib_version_data version_data = {
 	PACKAGE_VERSION,
