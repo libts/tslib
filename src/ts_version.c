b5d189023a5cd81a32f2bf329aa1364b45306f2b
Martin Kepplinger <martin.kepplinger@puri.sm>
2020-07-01 12:34:29 +0200

    tslib 1.22

--- src/ts_version.c
+++ src/ts_version.c
@@ -17,7 +17,7 @@
 #include "config.h"
 #include "tslib.h"
 
-#define LIBTS_DATESTAMP "[unreleased]"
+#define LIBTS_DATESTAMP "2020-07-01"
 
 static struct ts_lib_version_data version_data = {
 	PACKAGE_VERSION,
