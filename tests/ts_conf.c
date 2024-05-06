84cea499f6057606a2968c5772d3bf264892b3b0
Martin Kepplinger <martink@posteo.de>
2024-05-06 11:46:49 +0200

    tests/tools: convert license texts into SPDX license identifiers
    and replace deprecated ones with the stable identifiers, see
    https://spdx.org/licenses/

--- tests/ts_conf.c
+++ tests/ts_conf.c
@@ -5,20 +5,7 @@
  *
  * This file is part of tslib.
  *
- * ts_print_mt is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * ts_print_mt is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ts_print_mt.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0+
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  *
  * Test the ts_config_filter API. Change filters at runtime.
