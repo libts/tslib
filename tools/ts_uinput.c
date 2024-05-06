2b0b7a9f05ca4f020a207bc8a10bd3ceedea112c
Martin Kepplinger <martink@posteo.de>
2024-05-06 11:46:49 +0200

    tests/tools: convert license texts into SPDX license identifiers
    and replace deprecated ones with the stable identifiers, see
    https://spdx.org/licenses/

--- tools/ts_uinput.c
+++ tools/ts_uinput.c
@@ -5,22 +5,7 @@
  * Martin Kepplinger <martin.kepplinger@ginzinger.com>  2016-09-14
  * Melchior FRANZ <melchior.franz@ginzinger.com>  2015-09-30
  *
- * This file is part of tslib.
- *
- * ts_uinput is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * ts_uinput is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ts_uinput.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0+
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  *
  * ts_uinput daemon to generate (single- and multitouch) input events
