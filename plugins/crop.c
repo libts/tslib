342e6c6a94457b38f699e906fa3bf7185f102dd2
Martin Kepplinger <martink@posteo.de>
2024-05-06 11:46:14 +0200

    plugins/src: fix SPDX license identifiers to match the original license text

--- plugins/crop.c
+++ plugins/crop.c
@@ -3,20 +3,7 @@
  *
  *  Copyright (C) 2024 Martin Kepplinger-Novaković
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: LGPL-2.1
+ * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 #include <errno.h>
 #include <stdlib.h>
